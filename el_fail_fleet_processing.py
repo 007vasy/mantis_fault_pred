44f1e7f91a59c121a3beccf9124f8f03498387f0
vasy <007vasy@gmail.com>
2017-11-08 08:11:06 +0100

    batman code correction

--- el_fail_fleet_processing.py
+++ el_fail_fleet_processing.py
@@ -10,19 +10,16 @@ colnames = ["exclude", "exclude", "componentoperatingseconds", "exclude", "exclu
             "vehicle_serialnumber", "exclude", "errorviewdetaildata_id", "exclude", "exclude", "exclude", "exclude",
             "exclude", "exclude", "exclude", "exclude", "exclude", "exclude", "exclude", "exclude", "error_description",
             "exclude", "exclude", "exclude", "s_errorcode"]
-if flag:
+if flag == 1:
     path_to_file = '/media/vasy/Data/Doksik/Learn/thesis/fault_pred/xtra_All_errors_with_metainfo.txt'
-else:
-    path_to_file = '/home/vassb/fault_pred_data/xtra_All_errors_with_metainfo.txt'
-
-if flag:
     electric_errors = pd.read_csv(path_to_file,
                               sep="\t", nrows=5000, error_bad_lines=False, parse_dates=True, names=colnames, skiprows=1)
 else:
+    path_to_file = '/home/vassb/fault_pred_data/xtra_All_errors_with_metainfo.txt'
     electric_errors = pd.read_csv(path_to_file,
                                   sep="\t", error_bad_lines=False, parse_dates=True, names=colnames, skiprows=1)
 
-#print electric_errors.head()
+print electric_errors.head()
 
 cols = [c for c in electric_errors.columns if c.lower()[:7] != 'exclude']
 
@@ -31,7 +28,7 @@ electric_errors = electric_errors[cols]
 #print electric_errors.columns
 print electric_errors.describe()
 
-if flag:
+if flag == 1:
     electric_errors.to_csv("/media/vasy/Data/Doksik/Learn/thesis/fault_pred/electric_errors.csv")
 else:
     electric_errors.to_csv("/home/vassb/fault_pred_data/electric_errors.csv")
