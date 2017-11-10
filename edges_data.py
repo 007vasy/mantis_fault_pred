5f1e80a69525ac05f2f4319eb951c5bfac8951a1
vasy <007vasy@gmail.com>
2017-11-10 10:16:48 +0100

    edges FIX 2

--- edges_data.py
+++ edges_data.py
@@ -22,16 +22,16 @@ for chunk in pd.read_csv(path_to_electric_errors_csv, parse_dates=True, chunksiz
     print "chunk " + str(i) + " processing started!"
     if flag == 0:
         chunk = chunk[["error_occurred_timestamp","s_errorcode","vehicle_serialnumber"]]
+        chunk.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
         chunk = chunk.assign(fail_in=np.zeros(len(chunk.index), dtype=bool))
         chunk = chunk.assign(fail_out=np.zeros(len(chunk.index), dtype=bool))
-        chunk.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
         chunk.to_csv(edges_unordered)
         flag = 1
     else:
         chunk = chunk[["error_occurred_timestamp","s_errorcode","vehicle_serialnumber"]]
+        chunk.columns = ['timestamp', 'to_go_node', "vehicle_serialnumber"]
         chunk = chunk.assign(fail_in=np.zeros(len(chunk.index), dtype=bool))
         chunk = chunk.assign(fail_out=np.zeros(len(chunk.index), dtype=bool))
-        chunk.columns = ['timestamp', 'to_go_node', "vehicle_serialnumber"]
         chunk.to_csv(edges_unordered, mode='a', header=False)
     print "chunk " + str(i) + " has processed!"
     i = i + 1
@@ -42,27 +42,28 @@ print "process ready"
 sap_txt_fault = pd.read_csv(path_to_sap_txt_csv, parse_dates=True)
 
 sap_txt_fault_edges_in = sap_txt_fault[["service task(s) begin","exchange part number","serialnumber"]]
+sap_txt_fault_edges_in.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 sap_txt_fault_edges_in = sap_txt_fault_edges_in.assign(fail_in=np.ones(len(sap_txt_fault_edges_in.index), dtype=bool))
 sap_txt_fault_edges_in = sap_txt_fault_edges_in.assign(fail_out=np.zeros(len(sap_txt_fault_edges_in.index), dtype=bool))
-sap_txt_fault_edges_in.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 
 sap_txt_fault_edges_out = sap_txt_fault[["service task(s) end","exchange part number","serialnumber"]]
+sap_txt_fault_edges_out.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 sap_txt_fault_edges_out = sap_txt_fault_edges_out.assign(fail_in=np.zeros(len(sap_txt_fault_edges_out.index), dtype=bool))
 sap_txt_fault_edges_out = sap_txt_fault_edges_out.assign(fail_out=np.ones(len(sap_txt_fault_edges_out.index), dtype=bool))
-sap_txt_fault_edges_out.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
+
 #######################################################################################################################
 
 sap_xml_fault = pd.read_csv(path_to_sap_xml_csv, parse_dates=True)
 
 sap_xml_fault_edges_in = sap_xml_fault[["service task(s) begin","exchange part number","serialnumber"]]
+sap_xml_fault_edges_in.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 sap_xml_fault_edges_in = sap_xml_fault_edges_in.assign(fail_in=np.ones(len(sap_xml_fault_edges_in.index), dtype=bool))
 sap_xml_fault_edges_in = sap_xml_fault_edges_in.assign(fail_out=np.zeros(len(sap_xml_fault_edges_in.index), dtype=bool))
-sap_xml_fault_edges_in.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 
 sap_xml_fault_edges_out = sap_xml_fault[["service task(s) end","exchange part number","serialnumber"]]
+sap_xml_fault_edges_out.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 sap_xml_fault_edges_out = sap_xml_fault_edges_out.assign(fail_in=np.zeros(len(sap_xml_fault_edges_out.index), dtype=bool))
 sap_xml_fault_edges_out = sap_xml_fault_edges_out.assign(fail_out=np.ones(len(sap_xml_fault_edges_out.index), dtype=bool))
-sap_xml_fault_edges_out.columns = ['timestamp','to_go_node',"vehicle_serialnumber"]
 
 #######################################################################################################################
 sap_edges = pd.concat([sap_txt_fault_edges_in,sap_txt_fault_edges_out,sap_xml_fault_edges_in,sap_xml_fault_edges_out],ignore_index = True)
